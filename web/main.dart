15756024d6efb22d659266069a39435b1a4b45bb
Dmitry Buzdin <buzdin@gmail.com>
2013-07-11 17:25:16 +0300

    google map example added;

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,28 @@
+import 'dart:html';
+import 'package:js/js.dart' as js;
+import 'package:google_maps/google_maps.dart';
+
 // This is Dart Web Application bootstrap
 
 void main() {
+
+  js.context.google.maps.visualRefresh = true;
+
+  final mapOptions = new MapOptions()
+  ..zoom = 13
+  ..center = new LatLng(56.946843515558456, 24.13162512207032)
+  ..mapTypeId = MapTypeId.ROADMAP
+  ;
+  final map = new GMap(query("#map_canvas"), mapOptions);
+  js.retain(map);
+
+  map.onCenterChanged.listen((ignore) {
+    print(map.center);
+  });
+
+  map.onZoomChanged.listen((ignore) {
+    print(map.zoom);
+  });
 }
 
 void sayHello() {
