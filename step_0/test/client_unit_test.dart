529d4d9898a0d7aa8aaa859a956d3bd7020c2c96
Dmitry Buzdin <buzdin@gmail.com>
2013-07-18 21:50:05 +0300

    Step-0 ready;

---
+++ step_0/test/client_unit_test.dart
@@ -0,0 +1,15 @@
+import 'package:unittest/unittest.dart';
+import '../web/main.dart';
+
+void main() {
+
+  test("hello world", () {
+    print('Hello, Dart!');
+  });
+
+  test("2 + 2 = 4", () {
+    var result = add(2, 2);
+    expect(result, equals(4));
+  });
+
+}
---
+++ step_0/web/main.dart
@@ -0,0 +1,10 @@
+import 'dart:html';
+
+// This is Dart Web Application bootstrap
+void main() {
+  print("Hello, Dart!");
+}
+
+int add(x, y) {
+  return x + y;
+}
