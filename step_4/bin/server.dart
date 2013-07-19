ebebd8ac998b08c9bde59b5df54d9782119731c1
Dmitry Buzdin <buzdin@gmail.com>
2013-07-19 19:21:54 +0300

    Step-4 initial commit;

---
+++ step_4/bin/server.dart
@@ -0,0 +1,28 @@
+import 'package:fukiya/fukiya.dart';
+import 'dart:io';
+import 'dart:json';
+
+final database = [];
+
+main() {
+ new Fukiya()
+  ..get('/api/places', getHandler)
+  ..post('/api/places', postHandler)
+  ..staticFiles('web')
+  ..use(new FukiyaJsonParser())
+  ..listen('127.0.0.1', 8080);
+}
+
+void getHandler(FukiyaContext context) {
+  var near = context.params['near'];
+  print("Near: " + near);
+  context.jsonResponse(database);
+}
+
+void postHandler(FukiyaContext context) {
+  print("New place : " + context.parsedBody);
+  database.add(context.parsedBody);
+  context.send("OK");
+}
+
+
---
+++ step_4/web/main.dart
@@ -0,0 +1,91 @@
+import 'dart:html';
+import 'dart:json';
+import 'dart:async';
+import 'package:js/js.dart' as js;
+import 'package:google_maps/google_maps.dart';
+
+final DEFAULT_ICON = 'http://icons.iconarchive.com/icons/designbolts/monsters-university/128/Monsters-Character-Young-Mikes-icon.png';
+
+void main() {
+  var map = drawMap();
+  initialize(map);
+}
+
+void initialize(map) {
+  query('#name').value = 'My Place';
+  query('#icon').value = DEFAULT_ICON;
+
+  loadPlaces(map);
+
+  new Timer(new Duration(seconds:5), () {
+    print("timer triggered");
+    loadPlaces(map);
+  });
+}
+
+GMap drawMap() {
+  js.context.google.maps.visualRefresh = true;
+
+  final mapOptions = new MapOptions()..zoom = 13..center = new LatLng(56.946843515558456, 24.13162512207032)..mapTypeId = MapTypeId.ROADMAP;
+  final map = new GMap(query("#map_canvas"), mapOptions);
+  js.retain(map);
+
+  map.onCenterChanged.listen((ignore) {
+    print(map.center);
+  });
+
+  map.onZoomChanged.listen((ignore) {
+    print(map.zoom);
+  });
+
+  map.onClick.listen((event) {
+    print(event.latLng);
+    var name = query('#name').value;
+    var icon = query('#icon').value;
+
+    savePlace(name, icon, event.latLng);
+    drawMarker(map, name, icon, event.latLng);
+  });
+
+  return map;
+}
+
+void loadPlaces(map) {
+  HttpRequest.getString("/api/places").then((response) {
+    var points = parse(response);
+    for (final point in points) {
+      var latLng = new LatLng(point['loc'][0], point['loc'][1]);
+      drawMarker(map, point['name'], point['icon'], latLng);
+    }
+  });
+}
+
+void savePlace(name, icon, latLng) {
+  var place = {
+    "name" : name,
+    "icon" : icon,
+    "loc" : [latLng.lat, latLng.lng]
+  };
+
+  var json = stringify(place);
+
+  var request = new HttpRequest();
+  request.open('POST', '/api/places');
+  request.setRequestHeader("Content-Type", "application/json");
+  request.setRequestHeader("Content-Length", json.length.toString());
+  request.send(json);
+}
+
+void drawMarker(map, name, icon, latLng) {
+  var marker = new Marker(
+      new MarkerOptions()
+      ..position = latLng
+      ..map = map
+      ..title = name
+      ..icon = icon
+  );
+}
+
+int add(x, y) {
+  return x + y;
+}
